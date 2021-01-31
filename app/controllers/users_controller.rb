296510d508e1511321f6c7dcf60ed510ebc8cd8b
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-01-31 21:31:43 +0500

    Fill create method and modify new.html.erb accordingly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,18 @@ class UsersController < ApplicationController
     end
 
     def create
-        
+        @user = User.new(user_params)
+
+        if @user.save
+            redirect_to new_user_path
+        else
+            render :new
+        end
+    end
+
+    private
+    def user_params
+        params.require(:user).
+            permit(:username, :email, :password)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :username, presence: true, within: 6..20, too_long: 'pick a shorter name (<20)', too_short: 'pick a longer name (6>)'
+    validates :username, presence: true
     validates :email, presence: true
     validates :password, presence: true
 end
