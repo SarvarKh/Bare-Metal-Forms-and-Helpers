fb02f5962f0b77f36e2c729004196675fce49bc5
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-02-01 15:35:35 +0500

    Add show the single user's page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
         @users = User.all
     end
 
+    def show
+        @user = User.find(params[:id])
+    end
+
     def new
         @user = User.new
     end
