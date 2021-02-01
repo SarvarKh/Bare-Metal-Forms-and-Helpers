7bab2b222c00e18ed1e00f9c20a42c7e55345e84
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-02-01 15:51:33 +0500

    Complete editing the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,16 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
     end
 
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id]) 
+        @user.update(user_params)
+        redirect_to @user
+    end
+
     def new
         @user = User.new
     end
